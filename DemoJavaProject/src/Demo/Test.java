79001927b5bb02342e0b85512df5983ad93d9149
nthieu6 <nthieu6@NTHIEU-PC.ldap.cmcglobal.com.vn>
2019-12-20 14:05:51 +0700

    Add Java Object Oriented Demo

--- DemoJavaProject/src/Demo/Test.java
+++ DemoJavaProject/src/Demo/Test.java
@@ -1,3 +1,4 @@
+package Demo;
 
 public class Test {
 	public static void main(String[] args) {
---
+++ DemoJavaProject/src/InterfacesDemo/Animal.java
@@ -0,0 +1,6 @@
+package InterfacesDemo;
+
+interface Animal {
+	   public void eat();
+	   public void travel();
+	}
